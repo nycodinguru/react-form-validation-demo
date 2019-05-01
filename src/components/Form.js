dc177f091cb0008f30afe659f0f556bc7f19c116
Rashad Rose <rashadd.rose@gmail.com>
2019-05-01 11:52:26 -0400

    first commit

---
+++ src/components/Form.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Form = props => {
+    return (
+        <div id="Contact" className="Contact-form-container">
+            <div className="Contact-form"> 
+            <h2>Contact</h2>
+            <div className="Checkmark-container">
+                <svg className="Checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52"><circle className="Checkmark-circle" cx="26" cy="26" r="25" fill="none"/><path className="Checkmark-check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/></svg>
+                <h3 className="Checkmark-message"></h3>
+                </div>
+                <form id="Contact-form" className="Form" action="" method="Post">
+                    <div className="Name-fields">
+                        <input className="First-name" name="firstName" type="text" placeholder="First Name" onChange={props.handleChange}></input>
+                        <input className="Last-name" name="lastName" type="text" placeholder="Last Name" onChange={props.handleChange}></input>
+                    </div>
+                <div className="Other-fields">
+                        <input className="Email" name="email" type="text" placeholder="Email" onChange={props.handleChange}></input>
+                        <div id="Email"></div>
+                       
+                        <textarea id="Message" name="message" placeholder="Type your message here..." onChange={props.handleChange}></textarea>
+                       
+                        <div className="Count">
+                            <span id="Count">{props.count}</span>/{props.charLimit}
+                        </div>
+                </div>
+                    <div className="Button-parent">
+                        <button className="Button" type="submit" onClick={ (e) => props.handleSubmit(e)}>Send</button>
+                    </div>       
+                </form>
+            </div> 
+        </div>
+        
+    )
+    
+}
+
+export default Form;
---
+++ src/containers/Form-Container.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import Form from '../components/Form';
+
+export default class FormContainer extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            message: '',
+            invalidChars: false,
+            charLimit: 500,
+            count: 0
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        if (this.state.message.length === 0){
+            alert('Don\'t forget to type a message!');
+            if (this.state.message.length === 0){ document.querySelector('#Message').classList.add('Invalid-field');}
+        }
+        if (this.state.firstName.length === 0 || this.state.lastName.length === 0 || this.state.email.length === 0 || this.state.invalidChars){
+            alert('Please fill out all of the fields!');
+            if (this.state.firstName.length === 0){ document.querySelector('.First-name').classList.add('Invalid-field');}
+            if (this.state.lastName.length === 0){ document.querySelector('.Last-name').classList.add('Invalid-field');}
+            if (this.state.email.length === 0){ document.querySelector('.Email').classList.add('Invalid-field');}
+        }
+        if (document.querySelectorAll('.Invalid-field').length === 0 && this.state.message.length > 0) {
+            document.querySelector('.Form').classList.add('Contact-send-animation');
+            document.querySelector('.Checkmark-container').style.zIndex = 100;
+            document.querySelector('.Checkmark-message').textContent = 'Message sent.';
+            setTimeout(() =>{ 
+                document.querySelector('.Checkmark-message').classList.add('animation');
+                }, 300);
+            document.querySelector('.Checkmark').classList.add('animation');
+            document.querySelector('.Checkmark-circle').classList.add('animation');
+            document.querySelector('.Checkmark-check').classList.add('animation');
+        }
+    }
+
+    handleChange(e) {
+        const name = e.target.name;
+        const value = e.target.value;
+        const count = e.target.value.length;
+    
+        this.setState({ [name]: value.slice(0, 128) }, this.validate(name, count, e));
+        if (name === 'message' ) this.setState({ count: count});
+    }
+
+    validate(name, count, e) {
+        const messageField = document.getElementById('Message');
+        const countText = document.getElementById('Count');
+        const invalidChars = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '0', '+', ' ', '0', '1', '2', '3', '4', '5', '6','7', '8', '9', '/', '\\', '?', '>', '<', ':', ';', 
+    '"', '|', '{', '}', '|', '.', '_', '+', '='];
+
+        if (name !== 'message' && count === 0) {
+            document.querySelector(`input[name=${name}]`).classList.add('Invalid-field');
+        }
+        if (name !== 'message' && !this.state.invalidChars && count > 0) {
+            document.querySelector(`input[name=${name}]`).classList.remove('Invalid-field');
+        }
+        if (name === 'email') { 
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(e.target.value)){
+                document.querySelector(`input[name=${name}]`).classList.add('Invalid-field');
+                this.setState({ invalidChars: true});
+            } else {
+                document.querySelector(`input[name=${name}]`).classList.remove('Invalid-field');
+                this.setState({ invalidChars: false});
+            }
+        }
+        if (name === 'firstName' || name === 'lastName') {
+            for (let i = 0; i < invalidChars.length; i++){
+                if (e.target.value.includes(invalidChars[i])){
+                    document.querySelector(`input[name=${name}]`).classList.add('Invalid-field');
+                    this.setState({ invalidChars: true});
+                } else { this.setState({ invalidChars: false});}
+            }
+        }
+        if (name === 'message') {
+            if (this.state.message.length < this.state.charLimit){
+                countText.style.color = 'black'
+            }
+            if (e.target.value.length >= this.state.charLimit){
+                messageField.value = messageField.value.slice(0, this.state.charLimit);
+                countText.style.color = 'red';
+            }
+            if (this.state.message.length > 0){
+                document.querySelector('#Message').classList.remove('Invalid-field');
+            }
+            if (e.target.value.length === 0){
+                document.querySelector('#Message').classList.add('Invalid-field');
+            }
+        }
+        
+    }
+
+    render(){
+        return (
+            <Form 
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                firstName={this.state.firstName}
+                lastName={this.state.lastName}
+                email={this.state.email}
+                message={this.state.message}
+                count={this.state.count}
+                charLimit={this.state.charLimit}
+            />
+        
+        )
+    }
+};
